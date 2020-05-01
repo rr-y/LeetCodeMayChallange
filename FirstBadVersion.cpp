9f2169e6361680c85a41d0853ba99fe4cb80fce6
Rahul RajYadav <33296488+rr-y@users.noreply.github.com>
2020-05-01 22:15:33 +0530

    success
    First problem of may LeetCode Challange.

---
+++ FirstBadVersion.cpp
@@ -0,0 +1,20 @@
+// Problem Link --> https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
+// Idea -> Binary Search
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1, high = n;
+        int ans=-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
